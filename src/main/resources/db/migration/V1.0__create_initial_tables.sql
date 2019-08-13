835054c016b2b3cac320fb9658a9e523a0b59c78
Thomas Rowe <thomasrowe101@gmail.com>
2019-08-13 14:58:20 -0700

    Improved SQL scripts.

--- src/main/resources/db/migration/V1.0__create_initial_tables.sql
+++ src/main/resources/db/migration/V1.0__create_initial_tables.sql
@@ -7,9 +7,9 @@ CREATE TABLE "user" (
 
 CREATE TABLE list (
     uuid UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
-    created_by UUID REFERENCES "user"(uuid),
-    name VARCHAR(256),
-    description VARCHAR(256),
+    created_by UUID NOT NULL REFERENCES "user"(uuid),
+    name VARCHAR(256) NOT NULL,
+    description VARCHAR(256) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
--- src/main/resources/db/migration/V1.1__create_user_list_permission_table.sql
+++ src/main/resources/db/migration/V1.1__create_user_list_permission_table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE user_list_permission (
-    user_uuid UUID REFERENCES "user"(uuid) NOT NULL,
-    list_uuid UUID REFERENCES list(uuid) NOT NULL,
-    can_view BOOLEAN DEFAULT FALSE,
-    can_edit BOOLEAN DEFAULT FALSE,
-    can_delete BOOLEAN DEFAULT FALSE,
+    user_uuid UUID NOT NULL REFERENCES "user"(uuid),
+    list_uuid UUID NOT NULL REFERENCES list(uuid),
+    can_view BOOLEAN NOT NULL DEFAULT FALSE,
+    can_edit BOOLEAN NOT NULL DEFAULT FALSE,
+    can_delete BOOLEAN NOT NULL DEFAULT FALSE,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
